146bbbc165e4ce141fc750cb43759ecb85181019
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-03 17:30:31 -0500

    Added projects belongs to association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, foreign_key: :owner_id, class_name: :User
+
+  validates :name, :built_with, :description, :url, :price, :owner_id, presence: true
 end
