39376ee3934dad02614da4344da66b05aee605e4
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-03 15:53:45 -0500

    Added more specs to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true, presence: true
+    validates :fullname, :email, presence: true    
+    
 end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :model do
+
+    it 'validates User creation working' do
+        user = User.new({
+            username: 'frank10',
+            fullname: 'Frank Gator',
+            email: 'frank@gmail.com'
+        })
+        expect(user.valid?).to be(true)
+    end
+
+    it 'validates User username to be present' do
+        user = User.new({
+            fullname: 'Frank Gator',
+            email: 'frank@gmail.com'
+        })
+        expect(user.valid?).to be(false)
+    end
+
+    it 'validates User fullname to be present' do
+        user = User.new({
+            username: 'frank10',
+            email: 'frank@gmail.com'
+        })
+        expect(user.valid?).to be(false)
+    end
+
+    it 'validates User email to be present' do
+        user = User.new({
+            username: 'frank10',
+            fullname: 'Frank Gator',
+        })
+        expect(user.valid?).to be(false)
+    end
+
+    it 'validates User username uniqueness' do
+        user = User.create({
+            username: 'frank10',
+            fullname: 'Frank Gator',
+            email: 'frank@gmail.com'
+        })
+
+        user1 = User.create({
+            username: 'frank10',
+            fullname: 'Frank Gator',
+            email: 'frank@gmail.com'
+        })
+        expect(user1).to be_invalid
+    end
+    
+end
