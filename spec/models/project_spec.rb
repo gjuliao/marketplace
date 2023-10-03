5f604231769a1a35cc26aad03f7381faa8e7c752
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-03 18:25:28 -0500

    Fixed project and user test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Created a new project' do
+    user = User.create!({
+      username: 'frank1100909asdf',
+      fullname: 'Frank Gator',
+      email: 'frank@gmail.com'
+  })
+
+    project = Project.create!({
+      name: 'Python',
+      built_with: 'python',
+      description: 'This is a python game',
+      url: 'www.google.com',
+      price: 30.0,
+      owner_id: user.id
+    })
+    expect(project.valid?).to be(true)
+  end
 end
