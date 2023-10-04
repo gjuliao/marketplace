33ced71a3106aa050dea1ac63700efced2f0aa27
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 10:34:48 -0500

    Added the order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_222555) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_153236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.float "value"
+    t.float "fee"
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_orders_on_project_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -35,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_222555) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "projects"
+  add_foreign_key "orders", "users"
   add_foreign_key "projects", "users", column: "owner_id"
 end
