635d66f0b44bccfee5626c37addab7d5f8f36340
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 14:22:15 -0500

    Added project create method controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+
+    def index
+        @projects = Project.all
+    end
+
+    def create
+        owner = current_user
+        @project = Project.new(project_params.merge(owner_id: owner))
+
+        if @project.save?
+            redirect_to projects_path, notice: 'Your project has been succesfully added!'
+        else
+            flash.now[:alert] = 'Failed to create project'
+            render :new
+        end
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :built_with, :description, :url)
+    end
+
 end
