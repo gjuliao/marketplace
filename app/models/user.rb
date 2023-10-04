882450fff24f5d023ca445443f2cba5cbe9cf5d7
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 11:07:30 -0500

    Edited user association with order

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true
     validates :fullname, :email, presence: true    
     has_many :projects
+    has_many :orders, foreign_key: :client_id, class_name: :Order
 end
