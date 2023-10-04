6cd82be59c485032e1cd5dc4097e957dc028a54d
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 17:56:11 -0500

    Fixed project create method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 class ProjectsController < ApplicationController
 
     def index
@@ -9,21 +12,20 @@ class ProjectsController < ApplicationController
     end
 
     def create
-        owner = current_user
-        @project = Project.new(project_params.merge(owner_id: owner))
-
-        if @project.save?
+        @project = Project.new(project_params)
+        
+        if @project.save
             redirect_to projects_path, notice: 'Your project has been succesfully added!'
         else
             flash.now[:alert] = 'Failed to create project'
-            render :new
+            redirect_to new_project_path
         end
     end
 
     private
 
     def project_params
-        params.require(:project).permit(:name, :built_with, :description, :url)
+        params.require(:project).permit(:name, :built_with, :description, :url, :price, :owner_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "projects#index"
 
-  resources :projects, only: %i[index create new]
+  resources :projects, only: [:index, :new, :create]
 end
