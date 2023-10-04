dd7cdf16d745f543a68b41267822dc234dfcab52
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 11:07:42 -0500

    Added order passing spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) do
+    User.create!({
+      username: 'frank1100909as',
+      fullname: 'Frank Gator',
+      email: 'frank@gmail.com'
+    })
+  end
+
+  let(:project) do
+    Project.create!({
+      name: 'Python',
+      built_with: 'python',
+      description: 'This is a python game',
+      url: 'www.google.com',
+      price: 30.0,
+      owner_id: user.id
+    })
+  end
+
+    it 'describe order created succesfully' do
+      order = Order.create!({
+        fee: 10,
+        value:20,
+        project_id: project.id,
+        client_id: user.id
+      })
+      expect(order.valid?).to be(true)
+  end
+
 end
