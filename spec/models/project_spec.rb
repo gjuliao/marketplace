d47de23e63b1e72b723fcbfd3a2f986bc1a1d93c
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-04 10:25:14 -0500

    Added specs to project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it 'Created a new project' do
-    user = User.create!({
+
+  let(:user) do
+    User.create!({
       username: 'frank1100909asdf',
       fullname: 'Frank Gator',
       email: 'frank@gmail.com'
-  })
+    })
+  end
 
+  it 'Created a new project' do
     project = Project.create!({
       name: 'Python',
       built_with: 'python',
@@ -18,4 +21,17 @@ RSpec.describe Project, type: :model do
     })
     expect(project.valid?).to be(true)
   end
+
+  it 'Project requires presence of name' do
+    project = Project.new({
+      built_with: 'python',
+      description: 'This is a python game',
+      url: 'www.google.com',
+      price: 30.0,
+      owner_id: user.id
+    })
+    expect(project.valid?).to be(false)
+  end
+
+  
 end
