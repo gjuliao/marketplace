a188b383b61b3b5004784c682686a25f5a83bf4f
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-06 15:42:06 -0500

    Added Order method of calculating fee

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
   belongs_to :project
   belongs_to :client, foreign_key: :client_id, class_name: :User
   validates :fee, :value, presence: true
+  before_create :calculate_fee
+
+  def calculate_fee
+    self.fee = project.price * 0.15
+    self.value = project.price
+  end
 end
