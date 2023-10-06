470c7c0cb1ee4ce84b15ab9117ab2ac7caef18d9
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-06 15:41:49 -0500

    Added Order create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+
+    def create
+        # @client = order_params[:client_id]
+        # @project = order_params[:project_id]
+        @order = Order.new(order_params)
+        @order.calculate_fee
+        
+        if @order.save
+            flash[:notice] = 'Your Order has been registered Succesfully!'
+            redirect_to projects_path
+        else
+            flash[:alert] = 'Failed to create order!'
+            redirect_to new_project_path
+        end
+    end
+
+
+    private
+
+    def order_params
+        params.require(:order).permit(:client_id, :project_id)
+    end
 end
