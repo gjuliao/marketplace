7520ab29d66529be02e0530def5a5399f1cfa1d8
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-06 14:54:08 -0500

    Edited the project new view with current_user

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    validates :username, uniqueness: true, presence: true
-    validates :fullname, :email, presence: true    
-    has_many :projects
-    has_many :orders, foreign_key: :client_id, class_name: :Order
-end
--- db/migrate/20231003222555_rename_user_to_owner_in_projects.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserToOwnerInProjects < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :projects, :user_id, :owner_id
-  end
-end
