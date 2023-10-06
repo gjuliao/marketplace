b2a09fa57e55fc0cad2d76e0d4435b81c8b721a4
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-06 14:44:19 -0500

    Added projects show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "projects#index"
 
-  resources :projects, only: [:index, :new, :create]
+  resources :projects, only: [:index, :new, :create, :show]
 end
