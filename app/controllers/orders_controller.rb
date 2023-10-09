cffffcd107c4aa8bbf6b0df15ebff35956c83401
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-09 11:55:11 -0500

    Fixed stripe bug and functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,25 @@
 class OrdersController < ApplicationController
 
+    def new
+    end
+
     def create
-        @order = Order.new(order_params)
+        @stripe_service = StripeService.new
+        @project = Project.find(order_params[:project_id])
+        @customer = User.find(order_params[:client_id])
+
+        unless @customer.present?
+            @customer = Customer.create(customer_params)
+        end
+
+        @stripe_customer = @stripe_service.find_or_create_customer(@customer)
+        @card = @stripe_service.create_stripe_customer_card(card_params, @stripe_customer)
+
+        @order = Order.new(client_id: order_params[:client_id], project_id: order_params[:project_id])
         @order.calculate_fee
+        @price = order_params[:value]
+
+        @charge = @stripe_service.create_stripe_charge(@price, @stripe_customer.id, @card.id, @project)
 
         if @order.save
             flash[:notice] = 'Your Order has been registered Succesfully!'
@@ -17,6 +34,11 @@ class OrdersController < ApplicationController
     private
 
     def order_params
-        params.require(:order).permit(:client_id, :project_id)
+        params.require(:order).permit(:fullname, :email, :client_id, :project_id, :value, :card_number, :exp_month, :exp_year, :cvv)
+    end
+
+    def card_params
+        params.require(:order).permit(:fullname, :email, :card_number, :exp_month, :exp_year, :cvv)
+
     end
 end
--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -3,15 +3,15 @@ class StripeService
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     end
 
-    def find_or_create_customer(customer)
-        if customer.stripe_customer_id.present?
-            stripe_customer = Stripe::Customer.retrieve(customer.stripe_customer_id)
+    def find_or_create_customer(user)
+        if user.stripe_customer_id.present?
+            stripe_customer = Stripe::Customer.retrieve(user.stripe_customer_id)
         else
             stripe_customer = Stripe::Customer.create({
-                name: customer.fullname,
-                email: customer.email,
+                name: user.fullname,
+                email: user.email,
             })
-            customer.update(stripe_customer_id: stripe_customer.id)
+            user.update(stripe_customer_id: stripe_customer.id)
         end
         stripe_customer
     end
@@ -24,13 +24,13 @@ class StripeService
         )
     end
 
-    def create_stripe_charge(amount_to_be_paid, stripe_customer_id, card_id, event)
+    def create_stripe_charge(price, stripe_customer_id, card_id, project)
         Stripe::Charge.create({
-            amount: amount_to_be_paid * 100,
+            amount: price.to_f.round.to_i * 100,
             currency: 'usd',
             source: card_id,
             customer: stripe_customer_id,
-            description: "Amount $#{amount_to_be_paid} charged for #{event.name}",
+            description: "Amount $#{price} charged for #{project.name}",
           })
     end
 
