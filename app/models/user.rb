743d3ccf35569229cf95d18ea54e5476f133fb5d
Giovanni Juliao <gjuliao32@gmail.com>
2023-10-10 10:04:26 -0500

    Generated devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :username, uniqueness: true, presence: true
     validates :fullname, :email, presence: true    
     has_many :projects
